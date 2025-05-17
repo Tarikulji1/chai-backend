3943964cfdbfa58d2e38ee9ebdc413439482e25c
Tarikulji1 <islam.work.tarikul@gmail.com>
2025-05-17 19:52:10 +0530

    feat: implement user registration route and async error handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './.env'});
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import {app} from './app.js';
 
 dotenv.config({
     path: './.env'
@@ -11,7 +12,7 @@ dotenv.config({
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        console.log(`Server is running on port http://localhost:${process.env.PORT || 8000}`);
     });
     app.on("error", (error) => {
         console.error("Error: ", error);
